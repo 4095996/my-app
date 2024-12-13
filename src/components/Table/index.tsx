d25435f0690278e13c668c1693f3103572b0439f
asokolov07011983 <holland48672@gmail.com>
2024-12-13 15:15:50 +0300

    save changes

--- src/components/Table/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export const Table = () => {
-    return (
-        <p>11111111111111</p>
-    )
-}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import App from './App';
 
 import './index.css';
 import {Table} from "./components/Table";
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Table />
-  </React.StrictMode>
 );
